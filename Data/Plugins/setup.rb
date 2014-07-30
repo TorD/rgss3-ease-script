2deab37ea879491b9e27513cc901b6a053a40933
Galenmereth <tor.damian@gmail.com>
2014-07-31 01:02:09 +0200

    Updated plugins framework setup

--- Data/Plugins/setup.rb
+++ Data/Plugins/setup.rb
@@ -4,25 +4,22 @@ PRIORITY_PLUGINS = [
 ]
 
 # REQUIRED: This is the root plugin directory (the one this file resides in).
-ROOT_PATH = "Data/Plugins"
-
-# REQUIRED: This is the bootstrap filename that the Plugin module should look for in plugins.
-BOOTSTRAP_FILE = "bootstrap.rb"
+ROOT_PATH = "Plugins"
 
 # We now require the Plugins module
-load_script "#{ROOT_PATH}/plugins_module.rb"
+load_script "Data/#{ROOT_PATH}/plugins_module.rb"
 
 # REQUIRED: We set the above settings in the Plugins module
 Plugins.root_path = ROOT_PATH                     
-Plugins.bootstrap_file = BOOTSTRAP_FILE
 
 # REQUIRED: We load all the plugins found in the ROOT_PATH folder. The :order param includes the above specified PRIORITY_PLUGINS array.
 # If you don't have any need for prioritizing plugins, simply call it without any params, like the line commented out below
 # Plugins.load_plugins
 Plugins.load_plugins(
   :path => "*",
-  :order => PRIORITY_PLUGINS)
+  :order => PRIORITY_PLUGINS
+  )
 
 Plugins.package
 
-load_script "#{ROOT_PATH}/scripts.rb"
+load_script "Data/#{ROOT_PATH}/scripts.rb"
